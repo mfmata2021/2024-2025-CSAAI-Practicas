2123f979c982e16db0844ae602d76b5d006da2e6
mf.mata.2021 <mf.mata.2021@alumnos.urjc.es>
2025-04-23 23:13:33 +0200

    Avances js,html

---
+++ P4/memory-game.js
@@ -0,0 +1,102 @@
+const selectors = {
+    gridContainer: document.querySelector('.grid-container'),
+    tablero: document.querySelector('.tablero'),
+    movimientos: document.querySelector('.movimientos'),
+    timer: document.querySelector('.timer'),
+    comenzar: document.querySelector('button'),
+    dimensionSelect: document.getElementById('dimension'),
+    win: document.querySelector('.win')
+}
+
+const state = {
+    gameStarted: false,
+    flippedCards: 0,
+    totalFlips: 0,
+    totalTime: 0,
+    loop: null
+}
+
+const generateGame = () => {
+    const dimensions = parseInt(selectors.dimensionSelect.value)
+
+    //-- Nos aseguramos de que el número de dimensiones es par
+    // y si es impar lanzamos un error
+    if (dimensions % 2 !== 0) {
+        throw new Error("Las dimensiones del tablero deben ser un número par.")
+    }
+
+    //-- Creamos un array con los emojis que vamos a utilizar en nuestro juego
+    const img = ['🐶', '🐱', '🐭', '🐹', '🦊', '🐻',
+        '🐼', '🐨', '🐯', '🦁', '🐮', '🐷',
+        '🐸', '🐵', '🦄', '🐔', '🐧', '🐢'
+    ];
+
+    //-- Elegimos un subconjunto de emojis al azar, así cada vez que comienza el juego
+    // es diferente.
+    // Es decir, si tenemos un array con 10 emojis, vamos a elegir el cuadrado de las
+    // dimensiones entre dos, para asegurarnos de que cubrimos todas las cartas
+    const picks = pickRandom(img, (dimensions * dimensions) / 2)
+
+    //-- Después descolocamos las posiciones para asegurarnos de que las parejas de cartas
+    // están desordenadas.
+    const items = shuffle([...picks, ...picks])
+
+    //-- Vamos a utilizar una función de mapeo para generar 
+    //  todas las cartas en función de las dimensiones
+    const cards = `
+        <div class="tablero" style="grid-template-columns: repeat(${dimensions}, auto)">
+            ${items.map(item => `
+                <div class="card">
+                    <div class="card-front"></div>
+                    <div class="card-back">${item}</div>
+                </div>
+            `).join('')}
+       </div>
+    `
+
+    //-- Vamos a utilizar un parser para transformar la cadena que hemos generado
+    // en código html.
+    const parser = new DOMParser().parseFromString(cards, 'text/html')
+
+    //-- Por último, vamos a inyectar el código html que hemos generado dentro de el contenedor
+    // para el tablero de juego.
+    selectors.tablero.replaceWith(parser.querySelector('.tablero'))
+}
+
+const pickRandom = (array, items) => {
+    // La sintaxis de tres puntos nos sirve para hacer una copia del array
+    const clonedArray = [...array]
+    // Random picks va almacenar la selección al azar de emojis
+    const randomPicks = []
+
+    for (let index = 0; index < items; index++) {
+        const randomIndex = Math.floor(Math.random() * clonedArray.length)
+        // Utilizamos el índice generado al azar entre los elementos del array clonado
+        // para seleccionar un emoji y añadirlo al array de randompicks.
+        randomPicks.push(clonedArray[randomIndex])
+        // Eliminamos el emoji seleccionado del array clonado para evitar que 
+        // vuelva a salir elegido con splice.
+        // 0 - Inserta en la posición que le indicamos.
+        // 1 - Remplaza el elemento, y como no le damos un nuevo elemento se queda vacío.
+        clonedArray.splice(randomIndex, 1)
+    }
+
+    return randomPicks
+}
+const shuffle = array => {
+    const clonedArray = [...array]
+
+    // Intercambiamos las posiciones de los emojis al azar para desorganizar el array
+    // así nos aseguramos de que las parejas de emojis no están consecutivas.
+    // Para conseguirlo utilizamos un algoritmo clásico de intercambio y nos apoyamos
+    // en una variable auxiliar.
+    for (let index = clonedArray.length - 1; index > 0; index--) {
+        const randomIndex = Math.floor(Math.random() * (index + 1))
+        const original = clonedArray[index]
+
+        clonedArray[index] = clonedArray[randomIndex]
+        clonedArray[randomIndex] = original
+    }
+
+    return clonedArray
+}
